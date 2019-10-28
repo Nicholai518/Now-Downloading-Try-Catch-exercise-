6a34879125c9be6612e51704f7a375b0d76a56ce
Jagorre <40877736+Npaz44@users.noreply.github.com>
2019-10-28 00:49:00 -0400

    Add files via upload
    FORMATTED.

--- MovingBallForDownload.java
+++ MovingBallForDownload.java
@@ -5,108 +5,88 @@ import javax.swing.JOptionPane;
 public class MovingBallForDownload {
 
 	public static void main(String[] args) {
-		
+
 		downloadingAnimation();
 
-	}  // End of Main Method
-	
-	
-	
-	public static void downloadingAnimation()
-	{
-		
-		System.out.println("Now downloading...");
-				
-				try
-				{
-					Thread.sleep(600);
-					System.out.println("O");
-					Thread.sleep(200);
-					System.out.println(" O");
-					Thread.sleep(200);
-					System.out.println("  O");
-					Thread.sleep(200);
-					System.out.println("   O");
-					Thread.sleep(200);
-					System.out.println("    O");
-					Thread.sleep(200);
-					System.out.println("     O");
-					Thread.sleep(200);
-					System.out.println("      O");
-					Thread.sleep(200);
-					System.out.println("       O");
-					Thread.sleep(200);
-					System.out.println("        O");
-					Thread.sleep(200);
-					System.out.println("       O");
-					Thread.sleep(200);
-					System.out.println("      O");
-					Thread.sleep(200);
-					System.out.println("     O");
-					Thread.sleep(200);
-					System.out.println("    O");
-					Thread.sleep(200);
-					System.out.println("   O");
-					Thread.sleep(200);
-					System.out.println("  O");
-					Thread.sleep(200);
-					System.out.println(" O");
-					Thread.sleep(200);
-					System.out.println("O");
-					Thread.sleep(200);
-					System.out.println(" O");
-					Thread.sleep(200);
-					System.out.println("  O");
-					Thread.sleep(200);
-					System.out.println("   O");
-					Thread.sleep(200);
-					System.out.println("    O");
-					Thread.sleep(200);
-					System.out.println("     O");
-					Thread.sleep(200);
-					System.out.println("      O");
-					Thread.sleep(200);
-					System.out.println("       O");
-					Thread.sleep(200);
-					System.out.println("        O");
-					Thread.sleep(200);
-					System.out.println("       O");
-					Thread.sleep(200);
-					System.out.println("      O");
-					Thread.sleep(200);
-					System.out.println("     O");
-					Thread.sleep(200);
-					System.out.println("    O");
-					Thread.sleep(200);
-					System.out.println("   O");
-					Thread.sleep(200);
-					System.out.println("  O");
-					Thread.sleep(200);
-					System.out.println(" O");
-					Thread.sleep(200);
-					System.out.println("O");
-					
-					Thread.sleep(600);
-					System.out.println("Download Complete!");
-				}
-				catch ( Exception e)
-				{
-					JOptionPane.showMessageDialog(null, "Nope its broke.");
-				}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	} // End of Main Method
 
-}  // End of Class
+	public static void downloadingAnimation() {
+
+		System.out.println("Now downloading...");
+
+		try {
+			Thread.sleep(600);
+			System.out.println("O");
+			Thread.sleep(200);
+			System.out.println(" O");
+			Thread.sleep(200);
+			System.out.println("  O");
+			Thread.sleep(200);
+			System.out.println("   O");
+			Thread.sleep(200);
+			System.out.println("    O");
+			Thread.sleep(200);
+			System.out.println("     O");
+			Thread.sleep(200);
+			System.out.println("      O");
+			Thread.sleep(200);
+			System.out.println("       O");
+			Thread.sleep(200);
+			System.out.println("        O");
+			Thread.sleep(200);
+			System.out.println("       O");
+			Thread.sleep(200);
+			System.out.println("      O");
+			Thread.sleep(200);
+			System.out.println("     O");
+			Thread.sleep(200);
+			System.out.println("    O");
+			Thread.sleep(200);
+			System.out.println("   O");
+			Thread.sleep(200);
+			System.out.println("  O");
+			Thread.sleep(200);
+			System.out.println(" O");
+			Thread.sleep(200);
+			System.out.println("O");
+			Thread.sleep(200);
+			System.out.println(" O");
+			Thread.sleep(200);
+			System.out.println("  O");
+			Thread.sleep(200);
+			System.out.println("   O");
+			Thread.sleep(200);
+			System.out.println("    O");
+			Thread.sleep(200);
+			System.out.println("     O");
+			Thread.sleep(200);
+			System.out.println("      O");
+			Thread.sleep(200);
+			System.out.println("       O");
+			Thread.sleep(200);
+			System.out.println("        O");
+			Thread.sleep(200);
+			System.out.println("       O");
+			Thread.sleep(200);
+			System.out.println("      O");
+			Thread.sleep(200);
+			System.out.println("     O");
+			Thread.sleep(200);
+			System.out.println("    O");
+			Thread.sleep(200);
+			System.out.println("   O");
+			Thread.sleep(200);
+			System.out.println("  O");
+			Thread.sleep(200);
+			System.out.println(" O");
+			Thread.sleep(200);
+			System.out.println("O");
+
+			Thread.sleep(600);
+			System.out.println("Download Complete!");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Nope its broke.");
+		}
+	}
+
+} // End of Class
